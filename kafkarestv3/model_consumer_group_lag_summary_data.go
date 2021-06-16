9744f9e04f525e9a07561be2b14b8cf3d84c5bff
Alyssa <ahuang@confluent.io>
2021-06-16 02:10:14 -0700

    int64 and related link

--- kafkarestv3/model_consumer_group_lag_summary_data.go
+++ kafkarestv3/model_consumer_group_lag_summary_data.go
@@ -20,8 +20,8 @@ type ConsumerGroupLagSummaryData struct {
 	MaxLagClientId    string           `json:"max_lag_client_id"`
 	MaxLagTopicName   string           `json:"max_lag_topic_name"`
 	MaxLagPartitionId int32            `json:"max_lag_partition_id"`
-	MaxLag            int32            `json:"max_lag"`
-	TotalLag          int32            `json:"total_lag"`
+	MaxLag            int64            `json:"max_lag"`
+	TotalLag          int64            `json:"total_lag"`
 	MaxLagConsumer    Relationship     `json:"max_lag_consumer"`
 	MaxLagPartition   Relationship     `json:"max_lag_partition"`
 }
